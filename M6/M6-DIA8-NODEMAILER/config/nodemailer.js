5758756485e5563b5dbd8c4c5c5c4e715abc9ceb
tablonimus <lautamartin1995@gmail.com>
2024-04-02 13:14:21 -0300

    nodemailer advanced 3.0 full

--- M6/M6-DIA8-NODEMAILER/config/nodemailer.js
+++ M6/M6-DIA8-NODEMAILER/config/nodemailer.js
@@ -4,17 +4,39 @@ const nodemailer = require("nodemailer");
 const MI_MAIL = "nodemailerADL@gmail.com";
 const MI_MAIL_PASSWORD = "vamoscontodo";
 
-
-
 /* Con crate transport le paso a nodemalier mis credenciales */
 let transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
+    type: "OAuth2",
     user: MI_MAIL,
     pass: MI_MAIL_PASSWORD,
+    clientId: "000000000000-xxx0.apps.googleusercontent.com",
+    clientSecret: "XxxxxXXxX0xxxxxxxx0XXxX0",
+    refreshToken: "1/XXxXxsss-xxxXXXXXxXxx0XXXxxXXx0x00xxx",
+  },
+});
+
+/* 
+OBJETO EJEMPLO
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    type: "OAuth2",
+    user: "user@example.com",
+    clientId: "000000000000-xxx0.apps.googleusercontent.com",
+    clientSecret: "XxxxxXXxX0xxxxxxxx0XXxX0",
+    refreshToken: "1/XXxXxsss-xxxXXXXXxXxx0XXXxxXXx0x00xxx" 
   },
 });
 
 
+
+
+
+
+
+
+*/
 
 module.exports = { transporter, MI_MAIL };
