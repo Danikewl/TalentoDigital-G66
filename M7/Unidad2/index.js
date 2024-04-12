c0e2bba93c7986fb25024e5782c35588c35ad683
tablonimus <lautamartin1995@gmail.com>
2024-04-12 13:51:29 -0300

    transacciones

---
+++ M7/Unidad2/index.js
@@ -0,0 +1,63 @@
+import pg from "pg";
+const { Pool } = pg;
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  password: "1234",
+  database: "dinero_azul",
+  port: 5432,
+});
+
+const transactionExample = async () => {
+  try {
+    // Paso inicial => Iniciar la transacción
+    console.log("BEGIN START");
+    await pool.query("BEGIN");
+
+    const descontar =
+      "UPDATE usuarios SET saldo = saldo - 10000 WHERE email = 'fghfghfghf@cox.net' RETURNING *";
+    const clienteDescontado = await pool.query(descontar);
+
+    const acreditar =
+      "UPDATE usuarios SET saldo = saldo + 10000 WHERE email = 'yuki_whsdfsdfsdfsdbrey@aol.com' RETURNING *";
+    const clienteAcreditado = await pool.query(acreditar);
+
+    if (!clienteAcreditado.rowCount || !clienteDescontado.rowCount) {
+      const rollback = "ROLLBACK";
+      await pool.query(rollback);
+      console.log({
+        status: "Error",
+        message: "La operación ha sido anulada",
+        code: 500,
+        emisor: clienteDescontado.rows[0],
+        receptor: clienteAcreditado.rows[0],
+      });
+    } else {
+      console.log({
+        status: "Error",
+        message: "Operación realizada con éxito.",
+        code: 200,
+        emisor: clienteDescontado.rows[0],
+        receptor: clienteAcreditado.rows[0],
+      });
+    }
+
+    // Paso final => Finalizar la transacción
+    await pool.query("COMMIT");
+    console.log("COMMIT END");
+
+    return { clienteAcreditado, clienteDescontado };
+  } catch (error) {
+    await pool.end();
+    return console.log({
+      message: error.message,
+      code: error.code,
+      detail: error.detail,
+      constraint: error.constraint,
+      mensajeDelProgramador: "¿Que estas haciendo?",
+    });
+  }
+};
+
+transactionExample();
